881789e875d3b24af31f7c054a72c6a5a94e6275
Tim Rodger <tim.rodger@gmail.com>
2015-04-01 00:39:44 +0100

    Adds dictionary classes

---
+++ src/Ace/DictionaryInterface.php
@@ -0,0 +1,22 @@
+<?php namespace Ace;
+
+/**
+* Dictionary interface provides access to query a dictionary using a pattern
+*/
+interface DictionaryInterface
+{
+    /**
+    * @return array of words (strings) of length $length that match pattern
+    */
+    public function find($pattern, $length);
+
+    /**
+    * @return array or words of length $length
+    */
+    public function words($length);
+
+    /**
+    * @return integer the length of the longest word
+    */
+    public function longestWord();
+}
---
+++ src/Ace/SortedDictionary.php
@@ -0,0 +1,65 @@
+<?php namespace Ace\Dictionary;
+
+use Ace\DictionaryInterface;
+
+/**
+* implements DictionaryInterface using a data grouped by word length
+*/
+class SortedDictionary implements DictionaryInterface
+{
+    /**
+    * @var array of Word data
+    */
+    protected $dict = [];
+
+    /**
+    * @var string
+    */
+    protected $file;
+
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
+
+    public function find($pattern, $length)
+    {
+        $this->load();    
+
+        // assume that $pattern contains ^ and $ chars
+        $words = $this->dict[$length];
+        preg_match_all('#'.$pattern.'#m', $words, $matches);
+        return $matches[0];
+    }
+
+    public function words($length)
+    {
+        $this->load();    
+
+        if (isset($this->dict[$length])){
+            $words = $this->dict[$length];
+            return explode("\n", $words);
+        } else {
+            return [];
+        }
+    }
+
+    public function longestWord()
+    {
+        $this->load();
+
+        $lengths = array_keys($this->dict);
+        if (!count($lengths)){
+            return 0;
+        }
+        sort($lengths);
+        return array_pop($lengths);
+    }
+
+    protected function load()
+    {
+        if (!count($this->dict)){
+            $this->dict = require($this->file);
+        }
+    }
+}
